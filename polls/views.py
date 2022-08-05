47d05d8138562e3e68f0849c6194e356534e3e8a
JingjieSong <xiaosong1964073486@163.com>
2022-08-05 16:56:33 +0800

    add a comma polls/url.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
+=======
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. Your're at the polls index.")
+>>>>>>> 60ca7f3 (add a comma polls/url.py)
