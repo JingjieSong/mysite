c08697ca0eda94537d999b54a33f25c5dd869277
JingjieSong <xiaosong1964073486@163.com>
2022-08-08 17:48:43 +0800

    add a comma polls/url.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,15 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-=======
 from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
->>>>>>> 60ca7f3 (add a comma polls/url.py)
     path("admin/", admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
-=======
+
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. Your're at the polls index.")
->>>>>>> 60ca7f3 (add a comma polls/url.py)
